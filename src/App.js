f261a19f01bc94d44ff59ad20b36f1dc9dd25702
hounenhounen <hounen.hounen@gmail.com>
2020-05-30 12:41:30 +0000

    second comit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Part2.Part2
         </p>
         <a
           className="App-link"
